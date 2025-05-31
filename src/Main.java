0ab17a2d6ba95d1136d13cf98ad6c07a04798385
Kalash <pruthi.kalash27@gmail.com>
2025-05-31 10:56:42 +0530

    name change

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import strategy.IPlayerStrategy;
 
 public class Main {
     public static void main(String[] args) {
-        IPlayerStrategy playerXStrategy = new HumanPlayerStrategy("Kalash");
-        IPlayerStrategy playerOStrategy = new HumanPlayerStrategy("Gopi");
+        IPlayerStrategy playerXStrategy = new HumanPlayerStrategy("Player X");
+        IPlayerStrategy playerOStrategy = new HumanPlayerStrategy("Player Y");
         TicTacToeGame game = new TicTacToeGame(playerXStrategy, playerOStrategy, 3, 3);
         game.play();
     }
